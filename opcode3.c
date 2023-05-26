9009c381045cb4cd9fa8ba60f915acce6fcc3a5e
ShehabNegm <shebonegm@gmail.com>
2023-05-25 18:17:36 -0700

    fix math functions

--- opcode3.c
+++ opcode3.c
@@ -16,7 +16,7 @@ void sub(stack_t **head, unsigned int line)
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line);
 		exit(EXIT_FAILURE);
 	}
-	sb = (*head)->n + (*head)->next->n;
+	sb = (*head)->n - (*head)->next->n;
 	(*head)->next->n = sb;
 
 	*head = tmp->next;
@@ -72,7 +72,7 @@ void _div(stack_t **head, unsigned int line)
 		fprintf(stderr, "L%d: division by zero\n", line);
 		exit(EXIT_FAILURE);
 	}
-	dv = (*head)->n / (*head)->next->n;
+	dv = (*head)->next->n / (*head)->n;
 	(*head)->next->n = dv;
 
 	*head = tmp->next;
@@ -103,7 +103,7 @@ void mod(stack_t **head, unsigned int line)
 		fprintf(stderr, "L%d: division by zero\n", line);
 		exit(EXIT_FAILURE);
 	}
-	md = (*head)->n % (*head)->next->n;
+	md = (*head)->next->n % (*head)->n;
 	(*head)->next->n = md;
 
 	*head = tmp->next;
